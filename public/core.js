7a1b51734780addb9430bca76a5ad3c881456932
martinkropf08@gmail.com <26d4-j27>
2014-05-10 00:48:18 +0200

    holy moly
    charts displaying bugs by priority

--- public/core.js
+++ public/core.js
@@ -1,23 +1,70 @@
 // public/core.js
 var jiraApp = angular.module('jiraApp', []);
-
+ 
 function mainController($scope, $http) {
 	$scope.formData = {};
 
-	// when landing on the page, get all todos and show them
-	$http.get('/api/jira')
-		.success(function(data) {
-			try {
-			  	var transformed = JSON.parse(data);
-			  // transformed is { p: 10 }
-				$scope.jira = transformed;
-				console.log(data);
-			} catch (e) {
-		  console.log("Parsing error:", e); 
-		}
+	$scope.getProject = function() {
 
-		})
+		//Call node method / pass project name
+		$http.post('/api/jira', $scope.formData)
+				.success(function(data) {
+					try {
+
+						//Parse response to json.. twice.. because fuck you, thats why
+					  	var transformed = JSON.parse(data);
+			 		   	obj = JSON.parse(transformed);
+						$scope.jira = obj;
+
+						//Loop through response object, increment priority. 
+						//TODO: change to switch, looks like shit
+						var arrayLength = obj.issues.length;
+						var trivial =0;						
+						var minor =0;
+						var major =0;
+						var critical =0;						
+						var blocker =0;						
+						for (var i = 0; i < arrayLength; i++){
+							if(obj.issues[i].fields.priority.id == 5){
+								trivial++;
+							}
+							if(obj.issues[i].fields.priority.id == 4){
+								minor++;
+							}
+							if(obj.issues[i].fields.priority.id == 3){
+								major++;
+							}
+							if(obj.issues[i].fields.priority.id == 2){
+								critical++;
+							}
+							if(obj.issues[i].fields.priority.id == 1){
+								blocker++;
+							}
+						}
+						
+					//create google charts		
+        			data = google.visualization.arrayToDataTable([
+			          ['Task', 'Hours per Day'],
+			          ['Blocker',       blocker],
+			          ['Critical',      critical],
+			          ['Major',			major],			          
+			          ['Minor',			minor],
+			          ['Trivial',       trivial]
+			        ]);
+			      
+			        // Create and draw the visualization.
+			        new google.visualization.PieChart(document.getElementById('visualization')).
+			            draw(data,  {title:"List of bugs by priority"});
+
+					} catch (e) {
+					//TODO: some fancy error reporting		
+				  console.log("Parsing error:", e); 
+				}
+				})
 		.error(function(data) {
+			//TODO: some fancy error reporting		
 			console.log('Error: ' + data);
 		});
+
+	};
 }
--- web.js
+++ web.js
@@ -2,6 +2,7 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
+var bodyParser = require('body-parser');
 
 app.use(logfmt.requestLogger());
 
@@ -9,28 +10,32 @@ app.use(logfmt.requestLogger());
   res.send('Hello World! Manigga ');
 });*/
 
+
+
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
 
 var Client = require('node-rest-client').Client;
-var options_auth ={user:"martin.kropf", password:""};
+var options_auth ={user:"martin.kropf", password:"26d4-j27"};
 client = new Client(options_auth);
 
 
-
+app.use(bodyParser());
 app.use(express.static(__dirname + '/public'));
 
 // routes ======================================================================
 
 // api ---------------------------------------------------------------------
 // get all todos
-app.get('/api/jira', function(req, res) {
-	client.get("https://blanc-noir.atlassian.net/rest/api/latest/search?jql=project%20%3D%20MAN%20AND%20priority%20%3D%20Trivial%20AND%20resolution%20%3D%20Unresolved%20ORDER%20BY%20key%20DESC", 
+app.post('/api/jira', function(req, res) {
+	console.log(req.body.text);
+	client.get("https://blanc-noir.atlassian.net/rest/api/latest/search?jql=project%20%3D%20"+req.body.text+"%20AND%20priority%20in%20(Blocker%2C%20Critical%2C%20Major%2C%20Minor%2C%20Trivial)%20AND%20resolution%20%3D%20Unresolved%20ORDER%20BY%20key%20DESC", 
         function(data, response){
         	res.json(data);
-		});	    
+        	
+		});
 });
 
 	// application -------------------------------------------------------------

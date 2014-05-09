1b3d43ffaf3e2a08f043c1fea648f9b04ffdbfde
martinkropf08@gmail.com <26d4-j27>
2014-05-09 15:57:18 +0200

    lulz

--- web.js
+++ web.js
@@ -6,10 +6,11 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.send('Hello World! Manigga ');
 });
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
